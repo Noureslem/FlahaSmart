6ffb5ac49272206e3037872bc7ef071ae82fd4ff
Nour <khediri.noureslem@esprit.tn>
2026-03-02 01:25:55 +0100

    .

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.flahasmarty {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;   // ✅ THIS LINE IS MANDATORY
-
-    opens com.example.flahasmarty to javafx.fxml;
-    exports com.example.flahasmarty;
-}
