201cd1004a0856c6cc0a37e7416a5243fd6c31ec
boshbosh2025 <mohamedbacharmhamed@gmail.com>
2026-03-03 23:21:12 +0100

    Mise à jour EmailService et ajout configuration vsc.xml

--- flahasmart/src/main/java/com/example/flahasmart/services/EmailService.java
+++ flahasmart/src/main/java/com/example/flahasmart/services/EmailService.java
@@ -15,8 +15,8 @@ public class EmailService {
         // Paramètres Gmail (à remplacer par vos identifiants)
         this.host = "smtp.gmail.com";
         this.port = "587";
-        this.username = "amaltr21@gmail.com";
-        this.password = "gtye laay yhrb sxzp"; // Mot de passe d'application
+        this.username = "mohamedbacharmhamed@gmail.com";
+        this.password = "hvuc fezh gggc qdmg"; // Mot de passe d'application
     }
 
     public void sendHtmlEmail(String to, String subject, String htmlContent) throws MessagingException {
